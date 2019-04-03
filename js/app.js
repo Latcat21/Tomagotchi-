8757363589074466beb1f5f300859356e719fd03
Brian <Brian@Brians-MacBook-Air.local>
2019-04-03 14:11:08 -0500

    commented

--- js/app.js
+++ js/app.js
@@ -20,9 +20,9 @@ class Tomagotchi {
     this.alive = true,
     this.age = 0
   }
+/* Button functions */
   feeder(){
     this.hunger--
-    //jq
     $('.hunger').text(game.currentPet.hunger)
     console.log(game.currentPet.hunger)
    }
@@ -33,50 +33,54 @@ class Tomagotchi {
     console.log(game.currentPet.boredom)
   }
 
-  fatigue(){
+
+fatigue(){
     this.sleepiness--
     $('.sleepiness').text(game.currentPet.sleepiness)
 
   }
+  /* timer functions */
+
   getHungry(){
     this.hunger++
     $('.hunger').text(game.currentPet.hunger)
   }
   getSleepy(){
-  this.sleepiness++
-  $('.sleepiness').text(game.currentPet.sleepiness)
+    this.sleepiness++
+    $('.sleepiness').text(game.currentPet.sleepiness)
   }
  getBored(){
    this.boredom++
    $('.boredom').text(game.currentPet.boredom)
  }
  
- getBigger(){
-   $('.pet-dog').css({
-     'height': '125px',
-     'width': '125px',
-     'margin-bottom': '100px'
-   })
-   
- }
- moveRight(){
-   $('.pet-dog').css({
-     'margin-left': "10%",
-   })
- }
- moveLeft(){
-   $('.pet-dog').css({
-     'margin-right': "10%",
-   })
- }
 
- 
  getOlder(){
    this.age++
   $('.age').text(game.currentPet.age)
   console.log(game.currentPet.age)
 
  }
+ /* movement Functions */
+ getBigger(){
+  $('.pet-dog').css({
+    'height': '125px',
+    'width': '125px',
+    'margin-bottom': '100px'
+  })
+  
+}
+moveRight(){
+  $('.pet-dog').css({
+    'margin-left': "10%",
+  })
+}
+moveLeft(){
+  $('.pet-dog').css({
+    'margin-right': "10%",
+  })
+}
+/* death functions */
 
  getDead(){
    this.alive = false;
@@ -87,27 +91,26 @@ class Tomagotchi {
  }
 
 }
-
-
+/* Gam object */
 
 const game = {
   created: false,
   timerHandle: null,
   currentPet: null,
   time: 0,
-  
- startTimer(){
+  /* Timer for the pet */
+ 
+  startTimer(){
+    /* This is were I instiate it */
     const myPet = new Tomagotchi()
     this.currentPet = myPet
     
-    
-    // jq -- update screem
-    
-    
     this.timerHandle = setInterval(() =>{
-      // this.timeRemaining--
+     
       this.time++
+      /* store 1000 miliseconds / 1 second */
       const timeOneSecond = this.time * 1000
+      /* call fuctions at certains times */
       if(timeOneSecond % 2000 === 0){
         game.currentPet.getHungry();
       }
@@ -120,7 +123,6 @@ const game = {
       if(timeOneSecond % 4000 === 0){
         game.currentPet.getBored();
       }
-      
       if(timeOneSecond % 3000 === 0){
         game.currentPet.getOlder()
       }
@@ -130,30 +132,32 @@ const game = {
       if(timeOneSecond  % 5000 === 0){
        game.currentPet.getBigger()
       }
-      
       if(game.currentPet.hunger >= 10 || game.currentPet.boredom >= 10 || game.currentPet.sleepinesss >= 10){
        game.currentPet.getDead()
       }
       
 }, 1000)
-    
- }
+  }
 }
+/* Event Listeners */
 
+
+/* Event listereners for interacting with the pet */
  $('.btn').on('click', (e) => {
+     /* store the the clicked value */
+    
      const $clicked = $(event.target)
   
     if($clicked.hasClass('feed')) {
     game.currentPet.feeder();
     } if($clicked.hasClass('lights')) {
      game.currentPet.fatigue()
-     
-     console.log('you clicked lights')
-    } if($clicked.hasClass('play')) {
+     } if($clicked.hasClass('play')) {
      game.currentPet.happiness()
-    
-  }
+   }
 })
+/* Eent listener to start the game */
+
 $('.button').on('click', (e) => {
   const name = $('#tom-name').val();
   game.currentPet = true;
